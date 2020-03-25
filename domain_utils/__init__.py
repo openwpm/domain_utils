d33d2056e8ea588d950ccbc483e647e30460c0bf
Sarah Bird <birdsarah@mozilla.com>
2020-03-25 17:06:51 -0500

    Bump version: 0.3.0 → 0.4.0

--- domain_utils/__init__.py
+++ domain_utils/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Sarah Bird"""
 __email__ = 'sbird@mozilla.com'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 from .domain_utils import *  # noqa
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mozilla/domain_utils',
-    version='0.3.0',
+    version='0.4.0',
     zip_safe=False,
 )

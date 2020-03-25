b523286e63362eb8b54a940b531687ad30ffd5a1
Sarah Bird <birdsarah@mozilla.com>
2020-03-24 19:41:20 -0500

    Correct refs to repo

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/birdsarah/domain_utils',
+    url='https://github.com/mozilla/domain_utils',
     version='0.2.0',
     zip_safe=False,
 )

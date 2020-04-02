7a68f996a2e6c2cbfcafc6fc6f3dacae3bae6ad8
Sarah Bird <birdsarah@mozilla.com>
2020-04-02 17:12:24 -0500

    Revert "Try skipping instead of xfailing"
    This reverts commit 48c0a2cf8ce4b85c18ac5c877bbf3738a8afd27c.

--- tests/test_domain_utils.py
+++ tests/test_domain_utils.py
@@ -107,16 +107,14 @@ def test_get_stripped_url_returns_port_if_present_and_use_netloc_false():
 
 port_no_schema_msg = """
 urlparse does not have a good way to handle a url with a port but no scheme."""
-
-
-@pytest.mark.skip(reason=port_no_schema_msg)
+@pytest.mark.xfail(reason=port_no_schema_msg)
 def test_get_stripped_with_port_when_no_scheme():
     url = 'my.example.com:8080/path/to/webapp.htm?aced=1'
     result = du.get_stripped_url(url)
     assert result == 'my.example.com:8080/path/to/webapp.htm'
 
 
-@pytest.mark.skip(reason=port_no_schema_msg)
+@pytest.mark.xfail(reason=port_no_schema_msg)
 def test_get_stripped_url_with_port_when_no_scheme_and_use_netloc_false():
     url = 'my.example.com:8080/path/to/webapp.htm?aced=1'
     result = du.get_stripped_url(url, use_netloc=False)

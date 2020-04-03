d19b9546be8e3b1533e07b6f67938ffd369c45ea
Sarah Bird <birdsarah@mozilla.com>
2020-04-02 22:18:45 -0500

    Add new tests for get_ps_plus_1 including failing.

--- tests/test_domain_utils.py
+++ tests/test_domain_utils.py
@@ -21,6 +21,25 @@ def test_get_ps_plus_one_on_relative_url():
     assert result is None
 
 
+def test_get_ps_plus_1_on_exotic_public_suffix():
+    assert du.get_ps_plus_1('http://foo.bar.website.apartments') == 'website.apartments'
+
+
+def test_get_ps_plus_1_on_data_url():
+    assert du.get_ps_plus_1("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAA") is None
+
+
+def test_get_ps_plus_1_on_fbsbx_example():
+    # apps.fbsbx.com is on the public sufix list (Apr 2, 2020)
+    assert du.get_ps_plus_1('http://foo.blah.apps.fbsbx.com') == 'blah.apps.fbsbx.com'
+    assert du.get_ps_plus_1('http://foo.blah.www.fbsbx.com') == 'fbsbx.com'
+
+
+def test_get_ps_plus_1_on_ip_addresses():
+    assert du.get_ps_plus_1('http://192.168.1.1') == '192.168.1.1'
+    assert du.get_ps_plus_1('http://127.0.0.1/foo.html') == '127.0.0.1'
+
+
 def test_get_stripped_url_params():
     url = 'https://my.domain.cloudfront.net?a=1&b=2'
     result = du.get_stripped_url(url)

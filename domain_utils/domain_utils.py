9edbee918565caff0fb1b325d99bc793a1b73c81
Yabir G <yabirgb@gmail.com>
2020-04-08 01:37:55 +0200

    Add get_scheme function

--- domain_utils/domain_utils.py
+++ domain_utils/domain_utils.py
@@ -236,3 +236,29 @@ def get_stripped_url(url, scheme=False, drop_non_http=False, use_netloc=True, ex
         loc_out=loc_out,
         path_out=path_out,
     )
+
+def get_scheme(url):
+
+    """
+    Given an url extract from it the scheme
+    
+    Parameters
+    ----------
+    url: string 
+        The URL from where we want to get the scheme
+    
+    Returns
+    ----------
+    string
+        Returns the scheme with a default of 'blank' if no schema is provided
+    """
+
+    BLANK = 'blank'
+
+    scheme = urlparse(url).scheme
+
+    if scheme:
+        return scheme
+    else:
+        return BLANK
+    

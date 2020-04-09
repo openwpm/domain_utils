83512c23197c7f99ece1d6ebbc57aa6e24b9e1ec
Yabir G <yabirgb@gmail.com>
2020-04-09 20:12:49 +0200

    Fix style in tests

--- tests/test_get_port.py
+++ tests/test_get_port.py
@@ -2,34 +2,44 @@ from domain_utils import get_port
 
 
 def test_no_port():
-    assert get_port('domain.net') == None
+    assert get_port('domain.net') is None
+
 
 def test_port_in_ip():
-    assert get_port('10.0.0.1:80/path/to/index.html') == None
+    assert get_port('10.0.0.1:80/path/to/index.html') is None
+
 
 def test_port_in_url():
-    assert get_port('example.com:80/path/to/index.html') == None
+    assert get_port('example.com:80/path/to/index.html') is None
+
 
 def test_port_in_domain():
-    assert get_port('example.com:5000') == None
-    
+    assert get_port('example.com:5000') is None
+
+
 def test_port_in_ip_strict():
     assert get_port('10.0.0.1:80/path/to/index.html', strict=True) == 80
+
 
 def test_port_in_url_strict():
     assert get_port('example.com:80/path/to/index.html', strict=True) == 80
 
+
 def test_port_in_domain_strict():
     assert get_port('example.com:5000', strict=True) == 5000
+
 
 def test_url_with_protocol_strict():
     assert get_port('ws://example.com:5000', strict=True) == 5000
 
+
 def test_url_with_protocol():
     assert get_port('ws://example.com:5000') == 5000
 
+
 def test_no_port_strict():
-    assert get_port('domain.net', strict=True) == None
+    assert get_port('domain.net', strict=True) is None
+
 
 def test_no_port_strict_protocol():
-    assert get_port('https://domain.net', strict=True) == None
+    assert get_port('https://domain.net', strict=True) is None
